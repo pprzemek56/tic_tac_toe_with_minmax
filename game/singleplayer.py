487a8c82461b9c8932494dd55a14fbe03833d41f
pprzemek56 <przemkopis@wp.pl>
2022-11-09 13:44:21 +0100

    implement structure of single player move in game

--- game/singleplayer.py
+++ game/singleplayer.py
@@ -1,4 +1,5 @@
 from globals import BOARD, print_board
+from multiplayer import make_move, valid_choice, check_win
 
 
 def single_game():
@@ -27,11 +28,37 @@ def single_game():
 
     if player == "O":
         computer = "X"
+        counter = 0
+        end = 8
     else:
         computer = "O"
+        counter = 1
+        end = 9
 
     print("GOOD LUCK!")
     print("EACH SQUARE IS NUMERATED FROM 0 TO 8")
     print("STARTING FROM TOP LEFT CORNER")
     print("YOU CAN NOT ACCESS TAKEN SQUARE!")
     print_board()
+
+    while True:
+        if counter % 2 == 0:
+            # player turn
+            try:
+                choice = int(input("PLAYER TURN: "))
+                if not valid_choice(choice):
+                    raise ValueError
+            except ValueError:
+                continue
+            make_move(choice, player)
+            print_board()
+            if check_win():
+                print("CONGRATULATIONS FOR PLAYER!!")
+                break
+        else:
+            # computer turn
+            ...
+        if counter == end:
+            print("DRAW!")
+            break
+        counter += 1
